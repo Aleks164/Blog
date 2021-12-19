524e3c320cd00c071c5b32bef678f47aee76437a
Aleks164 <alekseylehamail.ru>
2021-12-19 20:03:55 +0300

    slider click done

--- src/slide/carusel.js
+++ src/slide/carusel.js
@@ -1,91 +1,90 @@
 (function () {
-  function createCarusel() {   
-     
+  function createCarusel() {
+
     let el = document.querySelector(".carusel");
     let line = el.dataset.showSliderLine;
     let slideShow = el.dataset.buttonSliderShow;
     let prevNext = el.dataset.prevNext;
-
-    console.log(prevNext);
     let imgList = el.querySelectorAll("img");
-    let arr =[];
+    let arr = [];
     let i = 0;
-    imgList.forEach(function(elem){
-     arr.push(elem.outerHTML); 
+    imgList.forEach((elem) => {
+      arr.push(elem.outerHTML);
     });
-    let imgListArr = arr.map(function(el){
+    let imgListArr = arr.map((el) => {
       let q = el.match(/src="(.*[(\.jpg)+(\.png)+])"/);
       return q[1];
     });
-    if(prevNext==="on"){
+    if (prevNext === "on") {
       let prev = document.createElement("span");
       let next = document.createElement("span");
       prev.classList.add("slide", "prev");
-    next.classList.add("slide", "next");    
-    prev.innerHTML = "<";
-    next.innerHTML = ">";
-    next.addEventListener("click", nextB);
-    prev.addEventListener("click", prevB);    
-    el.appendChild(prev);
-    el.appendChild(next);
+      next.classList.add("slide", "next");
+      prev.innerHTML = "<";
+      next.innerHTML = ">";
+      next.addEventListener("click", nextB);
+      prev.addEventListener("click", prevB);
+      el.appendChild(prev);
+      el.appendChild(next);
     };
-    
-    if(line==="on"){
+
+    if (line === "on") {
       let imageSpanRow = document.createElement("div");
       imageSpanRow.id = "spanCont";
-      imgList.forEach(function(elem, index){
+      imgList.forEach((elem, index) => {
         let item = document.createElement("span");
-        item.style.width = 90/imgList.length + "%";
+        item.style.width = `${90 / imgList.length}%`;
         item.classList.add("item");
         item.dataset.spannumber = index;
-        item.addEventListener("click", function(ev){
+        item.addEventListener("click", () => {
+          document.querySelectorAll(".item").forEach((spanItem) => {
+            spanItem.style.backgroundColor = "#adc5db5c";
+          });
+          item.style.backgroundColor = "#f3d59d99";
           imgList[i].style.display = "none";
-          i=index;
+          i = index;
           imgList[i].style.display = "block";
-        el.style.background = `url("${imgListArr[i]}") 0% 0% / 100% 100% no-repeat, rgba(255, 255, 255, 0.5)`;
+
+          el.style.background = `url("${imgListArr[i]}") 0% 0% / 100% 100% no-repeat, rgba(255, 255, 255, 0.5)`;
         });
-        item.innerHTML = "";      
-        imageSpanRow.appendChild(item);      
-       });
-       el.appendChild(imageSpanRow); 
+        item.innerHTML = "";
+        imageSpanRow.appendChild(item);
+      });
+      el.appendChild(imageSpanRow);
     }
-    if(slideShow==="on"){
+    if (slideShow === "on") {
       let slide = document.createElement("span");
       slide.classList.add("slidebut");
       slide.innerHTML = "SlideShow";
       slide.addEventListener("click", SlideShow);
       el.appendChild(slide);
     }
-    
-    
-    
-    
-        
+    document.querySelectorAll(".item")[i].style.backgroundColor = "#ffffff82";
     imgList[i].style.display = "block";
     el.style.background = `url("${imgListArr[i]}") 0% 0% / 100% 100% no-repeat, rgba(255, 255, 255, 0.5)`;
-    
+
     function nextB() {
-      if(i+1>imgList.length-1){       
+      if (i + 1 > imgList.length - 1) {
         imgList[i].style.display = "none";
-        i=0;
+        i = 0;
         imgList[i].style.display = "block";
         el.style.background = `url("${imgListArr[i]}") 0% 0% / 100% 100% no-repeat, rgba(255, 255, 255, 0.5)`;
       }
-      else{        
+      else {
         imgList[i].style.display = "none";
         i++;
         imgList[i].style.display = "block";
         el.style.background = `url("${imgListArr[i]}") 0% 0% / 100% 100% no-repeat, rgba(255, 255, 255, 0.5)`;
-      }      
+      }
     }
     function prevB() {
-      if(i-1<0){  
+      if (i - 1 < 0) {
         imgList[i].style.display = "none";
-        i=imgList.length-1;
+        i = imgList.length - 1;
         imgList[i].style.display = "block";
         el.style.background = `url("${imgListArr[i]}") 0% 0% / 100% 100% no-repeat, rgba(255, 255, 255, 0.5)`;
       }
-      else{       
+      else {
         imgList[i].style.display = "none";
         i--;
         imgList[i].style.display = "block";
@@ -94,14 +93,14 @@
     }
     let intervalID;
     function SlideShow() {
-if (!intervalID){
-  intervalID = setInterval(nextB,2000);
-}
-     else {
-       clearInterval(intervalID);
-       intervalID = null; 
-     }
-    }   
+      if (!intervalID) {
+        intervalID = setInterval(nextB, 2000);
+      }
+      else {
+        clearInterval(intervalID);
+        intervalID = null;
+      }
+    }
   }
 
   window.addEventListener("load", createCarusel);
