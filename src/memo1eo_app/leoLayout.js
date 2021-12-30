3eae6b11629263d599dcbe003d114bc7111d674d
Aleks164 <alekseylehamail.ru>
2021-12-31 00:17:30 +0300

    set sass linter

--- src/memo1eo_app/leoLayout.js
+++ src/memo1eo_app/leoLayout.js
@@ -14,17 +14,24 @@ export default function makeLayout(body) {
     <hr id="hr1">
 </div>
 <div id="maindiv" class="maindiv">
-    <div onclick="clickOnElment(this)" id="b1" class="buttons "></div>
-    <div onclick="clickOnElment(this)" id="b2" class="buttons "></div>
-    <div onclick="clickOnElment(this)" id="b3" class="buttons "></div>
-    <div onclick="clickOnElment(this)" id="b4" class="buttons "></div>
-    <div onclick="clickOnElment(this)" id="b5" class="buttons "></div>
-    <div onclick="clickOnElment(this)" id="b6" class="buttons "></div>
-    <div onclick="clickOnElment(this)" id="b7" class="buttons "></div>
-    <div onclick="clickOnElment(this)" id="b8" class="buttons "></div>
-    <div onclick="clickOnElment(this)" id="b9" class="buttons "></div>
-    <div onclick="clickOnElment(this)" id="b10" class="buttons "></div>
-    <div onclick="clickOnElment(this)" id="b11" class="buttons "></div>
-    <div onclick="clickOnElment(this)" id="b12" class="buttons "></div>
+    <div id="b1" class="buttons "></div>
+    <div id="b2" class="buttons "></div>
+    <div id="b3" class="buttons "></div>
+    <div id="b4" class="buttons "></div>
+    <div id="b5" class="buttons "></div>
+    <div id="b6" class="buttons "></div>
+    <div id="b7" class="buttons "></div>
+    <div id="b8" class="buttons "></div>
+    <div id="b9" class="buttons "></div>
+    <div id="b10" class="buttons "></div>
+    <div id="b11" class="buttons "></div>
+    <div id="b12" class="buttons "></div>
 </div>`;
+  const buttinList = document.querySelectorAll(".buttons");
+  buttinList.forEach((el) => {
+    el.onclick = function () {
+      // eslint-disable-next-line no-undef
+      clickOnElment(this);
+    };
+  });
 }
